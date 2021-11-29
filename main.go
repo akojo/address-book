62737a2b8b396432b2d86c83feeae034ceb26848
Atte Kojo <atte.kojo@gmail.com>
2021-11-29 09:24:30 +0200

    feat: Upgrade to cloud compatibility

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	router := bunrouter.New()
 	router.GET("/", func(w http.ResponseWriter, req bunrouter.Request) error {
-		fmt.Fprintln(w, "hello, world")
+		fmt.Fprintln(w, "hello, cloud")
 		return nil
 	})
 
